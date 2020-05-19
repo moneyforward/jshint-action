6353e4d34f1fe6c69c34309c699a0f24894eb95a
Naoki Kimura <n.kimura.cap@gmail.com>
2020-05-19 15:22:18 +0900

    Use Code review action

--- dist/analyzer.d.ts
+++
@@ -1,6 +0,0 @@
-import { StaticCodeAnalyzer, Transformers } from '@moneyforward/sca-action-core';
-export default class Analyzer extends StaticCodeAnalyzer {
-    constructor(options?: string[]);
-    protected prepare(): Promise<unknown>;
-    protected createTransformStreams(): Transformers;
-}
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,38 @@
-import Analyzer from './analyzer';
+import stream from 'stream';
+import util from 'util';
+import StaticCodeAnalyzer, { AnalyzerConstructorParameter } from '@moneyforward/sca-action-core';
+import { transform } from '@moneyforward/stream-util';
 
-console.log('::echo::%s', process.env['RUNNER_DEBUG'] === '1' ? 'on' : 'off');
-(async (): Promise<void> => {
-  const files = process.env.INPUT_FILES || '.';
-  const options = JSON.parse(process.env.INPUT_OPTIONS || '[]');
-  const workingDirectory = process.env.INPUT_WORKING_DIRECTORY;
-  workingDirectory && process.chdir(workingDirectory);
-  const analyzer = new Analyzer(options);
-  process.exitCode = await analyzer.analyze(files);
-})().catch(reason => {
-  console.log(`::error::${String(reason)}`);
-  process.exit(1);
-});
+const debug = util.debuglog('@moneyforward/code-review-action-jshint-plugin');
+
+export default class Analyzer extends StaticCodeAnalyzer {
+  constructor(...args: AnalyzerConstructorParameter[]) {
+    super('npx', ['jshint'].concat(args.map(String)).concat(['--reporter', 'unix', '--verbose']), undefined, exitStatus => exitStatus === 0 || exitStatus === 2, undefined, 'JSHint');
+  }
+
+  protected prepare(): Promise<void> {
+    return Promise.resolve();
+  }
+
+  protected createTransformStreams(): stream.Transform[] {
+    return [
+      new transform.Lines(),
+      new stream.Transform({
+        objectMode: true,
+        transform: function (warning: string, _encoding, done): void {
+          debug('%s', warning);
+          const regex = /^(.+):(\d+):(\d+): (.+) \(((?:E|W|I)\d+)\)$/;
+          const [matches, file, line, column, message, code] = regex.exec(warning) || [];
+          done(null, matches && {
+            file,
+            line,
+            column,
+            message: `${message}${code ? ` (${code})` : ''}`,
+            severity: code.startsWith('E') ? 'Error' : 'Warning',
+            code
+          });
+        }
+      })
+    ];
+  }
+}
